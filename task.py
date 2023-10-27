e1983ded0a9cda45a8f88d5ce9e243ccb517d6a2
Maksat474 <konurbaevm3@gmail.com>
2023-10-27 17:06:58 +0600

    sdf

--- task.py
+++ task.py
@@ -1,3 +1,14 @@
 from art import tprint
+
 tprint("PYTHON")
 
+from class1 import HI
+
+
+def tptint(obj):
+    print(obj)
+
+
+if __name__ == "__main__":
+    hi_instance = HI("Maksat")
+    tptint(hi_instance)
